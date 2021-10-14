8192eb110a50faf422e28deab9a84daa9222df99
Oscar <gumball_junior@hotmail.com>
2021-10-14 13:31:22 -0500

    the last commit

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib import auth
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
@@ -17,5 +18,25 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
-            
-            return reverse('article_detail', args=[str(self.id)])
+
+        return reverse('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):  # new
+
+    article = models.ForeignKey(
+        Article, 
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
